7f71dc1b055222fb4e9e068714f76236a2c00c1a
Simon Michalke <simon.m.m@freenet.de>
2016-07-30 12:09:42 +0200

    created define.h, fixes #6 partially

---
+++ src/define.h
@@ -0,0 +1,16 @@
+#ifndef DEFINE_H_INCLUDED
+#define DEFINE_H_INCLUDED
+
+
+#define bdt double //Basic Data Type. Change this to float
+                   //or double if you want
+
+///Natur-Konstanten:
+const bdt p_t  = 12526;    //Tripelpunkt p
+const bdt T_t  = 63.14;     //Tripelpunk T
+const bdt d_H  = 5590;      //Verdampfungsenthalpie
+const bdt R_m  = 8.3144598; //universelle Gaskonstante
+const bdt c_wN2= 2.6;       //spezifische Waermekapazitaet Stickstoff
+
+
+#endif // DEFINE_H_INCLUDED
--- src/main.cpp
+++ src/main.cpp
@@ -4,17 +4,9 @@
 #include <string>
 #include <fstream>
 
+#include "define.h"
+
 using namespace std;
-
-#define bdt double //Basic Data Type. Change this to float
-                   //or double if you want
-
-///Natur-Konstanten:
-const bdt p_t  = 12526;    //Tripelpunkt p
-const bdt T_t  = 63.14;     //Tripelpunk T
-const bdt d_H  = 5590;      //Verdampfungsenthalpie
-const bdt R_m  = 8.3144598; //universelle Gaskonstante
-const bdt c_wN2= 2.6;       //spezifische Waermekapazitaet Stickstoff
 
 ///Konstanten unserer Kammer:
 bdt V       = 0.06252;   //Volumen der Kammer in m^3
